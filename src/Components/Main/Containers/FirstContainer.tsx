d97f49589dae3b81c02885ceff1d51a08d8d2568
romeiro-bru <bruna_romeiro@yahoo.com.br>
2022-09-26 14:06:07 -0300

    Fix imports

--- src/Components/Main/Containers/FirstContainer.tsx
+++ src/Components/Main/Containers/FirstContainer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import allContents from "./contentList";
 import images from './images';
 
--- src/Components/Main/Containers/ThirdContainer.tsx
+++ src/Components/Main/Containers/ThirdContainer.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import allContents from "./contentList";
 
 export function ThirdContainer() {
